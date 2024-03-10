014ef5c557ea46cd5436c8dfe0fa6a4eb4c3ce89
Maciej Wójcik <maciek.wojcik179@gmail.com>
2024-03-10 21:55:50 +0100

    failsave plugin example

---
+++ src/test/java/com/ematiej/maven_course/JavaHelloJunitIT.java
@@ -0,0 +1,11 @@
+package com.ematiej.maven_course;
+
+import org.junit.jupiter.api.Test;
+
+public class JavaHelloJunitIT {
+
+    @Test
+    void myFakeIntegrationTest() {
+        System.out.println("MY IT RUN VERY VERY SLOWLY");
+    }
+}
